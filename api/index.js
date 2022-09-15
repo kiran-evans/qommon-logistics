52d9ec191510d8b0bb3f49d2a32da40379a122a8
Kiran <thekiranevans@gmail.com>
2022-09-15 11:18:44 +0100

    KE: Setup mongo and connected with api

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// ----------------------
+//THIS LINE DICTATES WHAT DB YOU USE!
+// FOR TESTING -> path: './config/testconfig.env'  // FOR DEV DB -> path: './config/config.env'
+const configPath = './config/config.env';
+dotenv.config({ path: configPath }); 
+// ----------------------
+
+const port = process.env.PORT;
+
+const connectDB = require('./config/db');
+connectDB();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const server = app.listen(port, () => console.info(`Server started on port ${port}`));
+
+(configPath === './config/config.env') && console.warn("-- WARNING: Running development environment. NOT SAFE FOR TESTING. --");
+
+module.exports = server;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+            <button>Driver login</button>
+            <button>Dispatcher login</button>
+
+        </div>
+    )
+}
+
+export default Home;
