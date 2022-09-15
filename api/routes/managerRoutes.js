248e92ef0b13e3d9072e3c6228c7e9273e6d2dca
Kiran <thekiranevans@gmail.com>
2022-09-15 15:59:56 +0100

    KE: Login system now works

--- api/routes/managerRoutes.js
+++ api/routes/managerRoutes.js
@@ -7,9 +7,11 @@ const {
   createManager,
   updateManager,
   deleteManager,
+  loginManager
 } = require('../controller/managerController');
 
 router.route('/').get(getAllManagers).post(createManager);
 router.route('/:id&:username').delete(deleteManager).put(updateManager).get(getManagerByID).get(getManagerByUsername);
+router.route('/login').post(loginManager);
 
 module.exports = router;
