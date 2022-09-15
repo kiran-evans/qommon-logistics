310662fd66a24a9250f0aea750e4a945f6cc89da
Daniel C <d.corne@hotmail.com>
2022-09-15 16:15:17 +0100

    let me pull bro

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Home from "./components/Home";
+import DriverPage from './components/DriverPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <DriverPage />
     </div>
   );
 }
